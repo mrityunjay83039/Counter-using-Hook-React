e2261a56025fb4682a12112c69ea416bf0b98a85
mrityunjay mishra <mrityunjay.83039@gmail.com>
2023-01-04 17:48:49 +0530

    uploading all files first time

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+
+
 
 function App() {
+
+const state = useState();
+const [count, setCount] = useState(1);     // destructuring of use state 
+
+function IncNum(){
+  setCount(count+1);
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="wrapper">
+        <h2 style={{color:'blueviolet', textAlign:'center'}}>Counter using useState Hook</h2>
+        <h1> {count} </h1>
+        <button onClick = {IncNum}>Click Me</button>
+      </div>
+    </>
   );
 }
 
